acdb1b37a1e6bbfaaede3122d7d53f939443ba09
pablo molina <pmolina9876@gmail.com>
2022-02-09 21:30:18 -0300

    PM/ add documentation function 09/02/2022

--- game-01/res/dist/res.js
+++ game-01/res/dist/res.js
@@ -1,5 +1,11 @@
 let M = [2, 12, 5, 15, 18, 14, 0, 8];
 let N = 10;
+/**
+ * Function to obtain the two values within an array of numbers that added together give a specific value.
+ * @param M {number[]} array that contains the numbers to add to get ___`N`___
+ * @param N {number} number that represents the result of the sum to be found in ___`M`___
+ * @returns {number[]} array with the numbers that add up to ___`N`___ or an empty array in case of not finding a correct sum.
+ */
 const F = (M, N) => {
     if (!M.length)
         return [];
